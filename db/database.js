3e22b37e3ee0ad6e4573c45bdc16fb5f507cf42c
webgot <webgot.js@gmail.com>
2017-05-06 23:32:26 +0900

    openshift setting

--- db/database.js
+++ db/database.js
@@ -13,9 +13,35 @@ database.init = function(app, config){
 function connect(app, config){
 
     console.log('connection() 호출됨');
+    
+
+    let mongoUrl = config.db_url;
+    /*
+    let mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
+
+    if(mongoURL == null && process.env.DATABASE_SERVICE_NAME){
+      var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+          mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+          mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+          mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+          mongoPassword = process.env[mongoServiceName + '_PASSWORD'],
+          mongoUser = process.env[mongoServiceName + '_USER'];
+
+          if(mongoHost && mongoPort && mongoDatabase){
+            mongoURL = 'mongodb://';
+            if(mongoUser && mongoPassword){
+              mongoURL += mongoUser +':'+mongoPassword +'@';
+            }
+            mongoURL += mongoHost+':'+mongoPort +'/'+mongoDatabase;
+          }
+    }
+    */
      // mongodb connection
-  
-  mongoose.connect(config.db_url);
+     /*
+
+     */
+
+  mongoose.connect(mongoUrl);
   database.db = mongoose.connection;
 
   database.db.on('error', (err)=>{
